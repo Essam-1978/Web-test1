453e275e134ef589c268ebe12f9a0f85e1d99bed
Essam-1978 <essam.alibrahimi@gmail.com>
2021-10-23 15:49:20 +0300

    Add project files.

---
+++ Core/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace Core
+{
+    public class EntityBase
+    {
+        public Guid Id { get; set; }
+    }
+    
+}
---
+++ Core/Owner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Owner
+    {
+        public string FullName { get; set; }
+        public string Profile { get; set; }
+        public string Avatar { get; set; }
+
+        public Address Address { get; set; }
+    }
+}
